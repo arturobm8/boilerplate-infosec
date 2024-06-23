1ff7f5c48a80a03fceb21a51a4cd1afd5f92c00b
Arturo BM <a01168331@tec.mx>
2024-06-23 13:56:10 -0600

    Render
    Render

--- myApp.js
+++ myApp.js
@@ -2,9 +2,11 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+const PORT = process.env.PORT || 3030;
 
-
-
+app.listen(PORT, () => {
+  console.log(`FreeCodeCamp applicaction started on port ${PORT}.`);
+});
 
 
 
